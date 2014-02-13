32f781a3a1872f84d1e7807e8ad360edfae2f856
Randy Varela <randyvarela@Randys-MacBook-Pro.local>
2014-02-13 17:53:26 -0500

    Initial Commit

---
+++ Man's Best Friend/RVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RVAppDelegate.h
+//  Man's Best Friend
+//
+//  Created by Randy Varela on 2/13/14.
+//  Copyright (c) 2014 Randy Varela. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Man's Best Friend/RVViewController.h
@@ -0,0 +1,13 @@
+//
+//  RVViewController.h
+//  Man's Best Friend
+//
+//  Created by Randy Varela on 2/13/14.
+//  Copyright (c) 2014 Randy Varela. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RVViewController : UIViewController
+
+@end
